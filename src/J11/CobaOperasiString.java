84d4669c30ae41ecaaddd99f92f7014d5fb54e44
WINDOWS 10 <WINDOWS 10@DESKTOP-38EJHUP>
2018-08-10 08:30:05 +0700

    Initial Commit

---
+++ src/J11/CobaOperasiString.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J11;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class CobaOperasiString {
+    public static void main(String[] args)
+    {
+        String identitas = "KINARA FAKHRUNNISA / XRPL3 / 16";
+        System.out.println("Identitas : " + identitas);
+    }
+}
