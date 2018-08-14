2c6b2b4212c4eab6f3ca4846531e8f9285f8f3b8
WINDOWS 10 <WINDOWS 10@DESKTOP-38EJHUP>
2018-08-14 12:20:53 +0700

    Initial Commit

---
+++ src/J11/ConvertVocal2Angka.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package J11;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+         System.out.println("\nConvert Kalimat Alay Angka (Vokal ke Angka)\n");
+    } 
+    public static void main(String[] args){
+    String identitas = "Kinara Fakhrunnisa / XR3 / 16";
+    tampilJudul(identitas);
+    
+}
+}
