f724f1a27b0288033c0345028dc44675594a0682
WINDOWS 10 <WINDOWS 10@DESKTOP-38EJHUP>
2018-08-16 19:20:34 +0700

    Coba method format

--- src/J11/CobaOperasiString.java
+++ src/J11/CobaOperasiString.java
@@ -5,6 +5,8 @@
  */
 package J11;
 
+import java.util.Locale;
+
 /**
  *
  * @author WINDOWS 10
@@ -81,6 +83,12 @@ public class CobaOperasiString {
         System.out.println("pecah berdasarkan huruf 'r' di variabel tclean : ");
         for (int i = 0; i < arrT.length; i++) 
             System.out.println((arrTr[i]));
+        
+        double d = 1545454000;
+        System.out.println(String.format("Uang saya %,.4f",d));
+        
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"),
+                "Uang saya Rp %,.2f",d));
         }
     }
 
